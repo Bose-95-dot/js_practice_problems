afcb8cbd7eb7f1c6060226ac56ee2318567a5f9c
Sudipta Bose <149366183+SBosee@users.noreply.github.com>
2024-11-24 17:36:10 +0530

    code problems added

--- JS_interview_qa.js
+++ JS_interview_qa.js
@@ -1,5 +1,6 @@
 //Interview Questions for Beginner JavaScript Developers
 //Q1. What will be the output of the below code:
+//code
 const array = [10, 20, 30, 40];
 const result = array.map((num) => num / 2).filter((num) => num >= 15);
 
@@ -10,6 +11,7 @@ const result = array.map((num) => num / 2).filter((num) => num >= 15);
 //In this case, the map() method is used to divide each element by 2, and the filter() method is used to keep only the elements that are greater than or equal to 15.
 
 //Q2. What will be the output of the below code:
+//code
 let counter = 0;
 for (var i = 1; i <= 10; i++) {
   counter+= i;
@@ -23,6 +25,7 @@ for (var i = 1; i <= 10; i++) {
 //The for loop iterates from i=1 to i=10, adding each value of i to the counter variable. At the end of the loop, the counter variable contains the sum of all numbers from 1 to 10, which is 55.
 
 //Q3. Analyze the below code. Do you see any issue? If yes, what is that issue?
+
 const object1 = {
     a: 10,
     b: 20,
@@ -50,6 +53,7 @@ const object1 = {
 // Ans: 80
 
   //q5. What will output of the below code?
+  //code
   function greetHello(name) {
     return `Hello, ${name}!`;
   }
@@ -73,4 +77,80 @@ const object1 = {
   });
   //Ans: url is not valid that's why it will show fetch failed
 
+//q7. What is the issue in the below code:
 
+const obj = {
+  name: "Conner",
+  age: 27,
+  greet: () => {
+	console.log(`Hey, my name is ${this.name}`);
+  },
+};
+obj.greet();
+
+//Ans: 
+//method 1
+// in console it will show undefined cause arrow function inherits this from surounding context which will be global.
+// To solve this we can use regular function instead of arrow.
+
+//method 2
+// we can use bind method to bind this to obj object
+// obj.greet.bind(obj)();
+
+
+//q8. What will be the output of the below code?
+//code
+const object8 = {
+    prop1: "value1",
+    prop2: {
+      prop3: "value3",
+    },
+  };
+   const newObj = { ...object8 };
+  newObj.prop2.prop3 = "newValue3";
+//   console.log(object8.prop2.prop3);
+  //ans: newValue3
+  //because of shallow coping of object  newObj and object8 will have same reference, that's why it will show newValue3.solution of this problem
+  // will be deep copy using this code const newObj = JSON.parse(JSON.stringify(object8))
+
+  //q9. Will the below code return any error? If yes, what will be the error?
+
+  //code
+  class Bird {
+    constructor(name) {
+      this.name = name;
+    }
+    speak() {
+      console.log(`${this.name} makes a noise.`);
+    }
+  }
+   class Crow extends Bird{
+    speak() {
+      super.speak();
+      console.log(`${this.name} sings.`);
+    }
+  }
+   const crow = new Crow("Tim");
+  crow.speak();
+
+  //Ans:
+  // it will show "Time makes a noise". and next line it will show  "Time sings".
+  //super.speak() correctly calls the speak method from the parent class Bird because Crow inherits from Bird.
+  // then Crow's own speak() method is called, which logs "Time sings."
+
+  //q10. Develop a function that throttles another function, allowing it to be called at most once every specified interval (e.g., 300ms). 
+  //The throttling function will have two input parameters. One will be the function to be throttled and the second will be the interval in milliseconds. 
+  //The throttled function should be called with the same arguments as the original function.
+
+
+
+  //q11. What is wrong with the below code?
+
+  //code
+  const arr = [1, 2, 3, 4, 5];
+const sum = arr.reduce((total, num) => total + num);
+console.log(sum / arr.length);
+
+//ans: 3
+//it will show 3 because reduce method will return the sum of all elements in the array and then divide by the length of the array.
+  
