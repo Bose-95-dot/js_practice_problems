f591b99fab79f85fd9237c53029d8d47c71ee578
Sudipta Bose <149366183+SBosee@users.noreply.github.com>
2025-02-09 09:32:52 +0530

    JS begineer level interview q/a added

--- JS_interview_qa.js
+++ JS_interview_qa.js
@@ -1,8 +1,8 @@
 //Interview Questions for Beginner JavaScript Developers
 //Q1. What will be the output of the below code:
 //code
-const array = [10, 20, 30, 40];
-const result = array.map((num) => num / 2).filter((num) => num >= 15);
+// const array = [10, 20, 30, 40];
+// const result = array.map((num) => num / 2).filter((num) => num >= 15);
 
 // Ans: [15,20]
 //explanatinon: 
@@ -12,10 +12,10 @@ const result = array.map((num) => num / 2).filter((num) => num >= 15);
 
 //Q2. What will be the output of the below code:
 //code
-let counter = 0;
-for (var i = 1; i <= 10; i++) {
-  counter+= i;
-}
+// let counter = 0;
+// for (var i = 1; i <= 10; i++) {
+//   counter+= i;
+// }
 // console.log(counter);
 // console.log(i)
 //formula:use this formula N*(N+1)/2
@@ -26,15 +26,15 @@ for (var i = 1; i <= 10; i++) {
 
 //Q3. Analyze the below code. Do you see any issue? If yes, what is that issue?
 
-const object1 = {
-    a: 10,
-    b: 20,
-    c: function () {
-      console.log(this.a + this.b);
-    },
-  };
-  const func = object1.c;
-  func.call(object1);
+// const object1 = {
+//     a: 10,
+//     b: 20,
+//     c: function () {
+//       console.log(this.a + this.b);
+//     },
+//   };
+//   const func = object1.c;
+//   func.call(object1);
 
   //Ans: 30
   //explanation: 
@@ -44,11 +44,11 @@ const object1 = {
 
   //Q4. Create a JavaScript function that calculates the tip for a given bill amount and tip percentage. Bill amount and tip percentage will be input parameters while output will be calculated tip value.
 
-  function calculateTip(billAmount, tipPercentage) {
-    const tip = (billAmount * tipPercentage) / 100;
-    return tip;
-  }
-  const tip = calculateTip(4000, 2);
+  // function calculateTip(billAmount, tipPercentage) {
+  //   const tip = (billAmount * tipPercentage) / 100;
+  //   return tip;
+  // }
+  // const tip = calculateTip(4000, 2);
 //   console.log(tip);
 // Ans: 80
 
@@ -62,31 +62,31 @@ const object1 = {
   // Ans: Hello, Brian!
 
   //q6. Will the below code return any error? If yes, identify the error.
-  function fetchData(callback) {
-    fetch('https://api.example.com/data')
-      .then(response => response.json())
-      .then(data => callback(null, data))
-      .catch(error => callback(error));
-  }
-     fetchData(function (error, data) {
-    if (error) {
-      console.log('Error:', error);
-    } else {
-      console.log('Data:', data);
-    }
-  });
+  // function fetchData(callback) {
+  //   fetch('https://api.example.com/data')
+  //     .then(response => response.json())
+  //     .then(data => callback(null, data))
+  //     .catch(error => callback(error));
+  // }
+  //    fetchData(function (error, data) {
+  //   if (error) {
+  //     console.log('Error:', error);
+  //   } else {
+  //     console.log('Data:', data);
+  //   }
+  // });
   //Ans: url is not valid that's why it will show fetch failed
 
 //q7. What is the issue in the below code:
 
-const obj = {
-  name: "Conner",
-  age: 27,
-  greet: () => {
-	console.log(`Hey, my name is ${this.name}`);
-  },
-};
-obj.greet();
+// const obj = {
+//   name: "Conner",
+//   age: 27,
+//   greet: () => {
+// 	console.log(`Hey, my name is ${this.name}`);
+//   },
+// };
+// obj.greet();
 
 //Ans: 
 //method 1
@@ -100,14 +100,14 @@ obj.greet();
 
 //q8. What will be the output of the below code?
 //code
-const object8 = {
-    prop1: "value1",
-    prop2: {
-      prop3: "value3",
-    },
-  };
-   const newObj = { ...object8 };
-  newObj.prop2.prop3 = "newValue3";
+// const object8 = {
+//     prop1: "value1",
+//     prop2: {
+//       prop3: "value3",
+//     },
+//   };
+//    const newObj = { ...object8 };
+//   newObj.prop2.prop3 = "newValue3";
 //   console.log(object8.prop2.prop3);
   //ans: newValue3
   //because of shallow coping of object  newObj and object8 will have same reference, that's why it will show newValue3.solution of this problem
@@ -116,22 +116,22 @@ const object8 = {
   //q9. Will the below code return any error? If yes, what will be the error?
 
   //code
-  class Bird {
-    constructor(name) {
-      this.name = name;
-    }
-    speak() {
-      console.log(`${this.name} makes a noise.`);
-    }
-  }
-   class Crow extends Bird{
-    speak() {
-      super.speak();
-      console.log(`${this.name} sings.`);
-    }
-  }
-   const crow = new Crow("Tim");
-  crow.speak();
+  // class Bird {
+  //   constructor(name) {
+  //     this.name = name;
+  //   }
+  //   speak() {
+  //     console.log(`${this.name} makes a noise.`);
+  //   }
+  // }
+  //  class Crow extends Bird{
+  //   speak() {
+  //     super.speak();
+  //     console.log(`${this.name} sings.`);
+  //   }
+  // }
+  //  const crow = new Crow("Tim");
+  // crow.speak();
 
   //Ans:
   // it will show "Time makes a noise". and next line it will show  "Time sings".
@@ -147,10 +147,22 @@ const object8 = {
   //q11. What is wrong with the below code?
 
   //code
-  const arr = [1, 2, 3, 4, 5];
-const sum = arr.reduce((total, num) => total + num);
-console.log(sum / arr.length);
+//   const arr = [1, 2, 3, 4, 5];
+// const sum = arr.reduce((total, num) => total + num);
+// console.log(sum / arr.length);
 
 //ans: 3
 //it will show 3 because reduce method will return the sum of all elements in the array and then divide by the length of the array.
   
+//q12. Find code outpute with reason.
+var x = Math.floor(Math.random());
+  console.log(x,"x")
+if(x > 0.5){
+var x = 1;
+}else{
+var x = 2;
+}
+console.log(x)
+
+output :
+2
